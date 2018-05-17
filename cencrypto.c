b633fc5d6340ce89b1b31abc7a17534429ca7032
ujjwalrox <ujjwalraizada@gmail.com>
2018-05-17 21:47:35 +0530

    Command line arguments accepted, random_hash included

--- cencrypto.c
+++ cencrypto.c
@@ -1,27 +1,61 @@
 #include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
 
 struct encrypto {
 	int c;
+	int random_hash;
 };
 
-char encryptor(char c){
+char encryptor(char c,char ecode){
 
-	return (c+5);
+	//Todo : Add a better encryption algorithm
+	return (c+ecode);
 }
-int main() {
-	
-	FILE *fpi,*fpo;
-	fpi = fopen("input.txt","r");
-	fpo = fopen("output","wb");
-	struct encrypto instance;
-	char c = getc(fpi);
-	while(c != EOF)
-	{
-		instance.c = encryptor(c);
-		fwrite(&instance, sizeof(struct encrypto), 1,fpo);
-		c = getc(fpi);
+
+int hash(char c,char ecode) {
+	/*A random_hash is used to make the output
+	more complicated to decrypt. */
+
+	//Todo : Improve this random hash function
+	return (int)pow(c,ecode);
+}
+
+int main(int argc, char *argv[]) {
+
+	if(argc != 3) {
+		printf("Two arguments expected.\n");
+		printf("cencrypto <input file> <output file>\n");
+		exit(1);
 	}
 
+	char inputfile[100], outputfile[100];
+	strcpy(inputfile,argv[1]);
+	strcpy(outputfile,argv[2]);
+
+	char password[100];
+	printf("Enter the password(Set up your encryption key) : ");
+	//Todo : Password should not be echoed back
+	scanf("%s",password);
+	
+	FILE *fpi,*fpo;
+	//Todo : add error handling, if file operation fails.
+	fpi = fopen(inputfile,"r");
+	fpo = fopen(outputfile,"wb");
+
+	struct encrypto instance;
+	char c = getc(fpi);
+	int passindex = 0;
+	while(c != EOF) {
+
+		instance.c = encryptor(c,password[passindex]);
+		instance.random_hash = hash(c,password[passindex]);
+		fwrite(&instance, sizeof(struct encrypto), 1,fpo);
+		c = getc(fpi);
+		passindex = (passindex+1)%strlen(password);
+	}
+	printf("encryption Completed!\n");
 	fclose(fpi);
 	fclose(fpo);
 	return 0;
--- decrypto.c
+++ decrypto.c
@@ -1,24 +1,44 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
 struct encrypto {
 	int c;
+	int random_hash;
 };
 
-char decryptor(char c){
+char decryptor(char c,char ecode){
 
-	return (c-5);
+	return (c-ecode);
 }
 
-int main() {
+int main(int argc,char *argv[]) {
 
-	FILE *fpi,*fpo;
-	fpi = fopen("output","rb");
-	fpo = fopen("decryped.txt","w");
-	struct encrypto instance;
-	while(fread(&instance, sizeof(struct encrypto), 1,fpi) == 1) {
-		fprintf(fpo,"%c",decryptor(instance.c));
+	if (argc != 3) {
+	printf("Two arguments expected.\n");
+	printf("decrypto <input file> <output file>\n");
+	exit(1);
 	}
 
+	char inputfile[100], outputfile[100];
+	strcpy(inputfile,argv[1]);
+	strcpy(outputfile,argv[2]);	
+
+	char password[100];
+	printf("Enter the password: ");
+	scanf("%s",password);
+
+	FILE *fpi,*fpo;
+	//Todo : add error handling, if file operation fails.
+	fpi = fopen(inputfile,"rb");
+	fpo = fopen(outputfile,"w");
+	struct encrypto instance;
+	int passindex = 0;
+	while (fread(&instance, sizeof(struct encrypto), 1,fpi) == 1) {
+		fprintf(fpo,"%c",decryptor(instance.c,password[passindex]));
+		passindex = (passindex+1)%strlen(password);
+	}
+	printf("Decryption Completed!\n");
 	fclose(fpi);
 	fclose(fpo);
 	return 0;
